63d9e43ae04ae47dcef7667d7b25ad55fc7a15c8
jhy9413 <54574536+jhy9413@users.noreply.github.com>
2020-10-23 18:18:21 +0900

    Update db.php

--- db.php
+++ db.php
@@ -2,6 +2,6 @@
         define("DB_HOST", "10.131.1.127"); //db서버
         define("DB_PORT", "3306");
         define("DB_USER", "root");
-        define("DB_PASSWORD", "");
+        define("DB_PASSWORD", "test123");
         define("DB_DATABASE", "dog");
 ?>
