a2b081ab1b56bf9c7da496aa93eac92a144097b5
jhy9413 <54574536+jhy9413@users.noreply.github.com>
2020-10-23 16:54:14 +0900

    Create db.php

---
+++ db.php
@@ -0,0 +1,7 @@
+<?php
+        define("DB_HOST", "10.131.1.127"); //db서버
+        define("DB_PORT", "3306");
+        define("DB_USER", "root");
+        define("DB_PASSWORD", "");
+        define("DB_DATABASE", "dog");
+?>
