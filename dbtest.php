eb1dd892637c25d44921d0bc2d503d2ffaf261cd
jhy9413 <54574536+jhy9413@users.noreply.github.com>
2020-10-23 15:53:55 +0900

    Update dbtest.php

--- dbtest.php
+++ dbtest.php
@@ -1,8 +1,9 @@
 <?php
         $host = '172.30.61.113';//db서버 ip
+        $port = '3306';
         $user = 'root';
         $pw = 'test123';
-        $dbName = 'mariadb-1-8r6t6';
+        $dbName = 'mariadb';
         $mysqli = new mysqli($host, $user, $pw, $dbName);
 
         if($mysqli){
