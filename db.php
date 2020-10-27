33b41ea41b8e0cc8685dd1fe98d75799043c999b
jhy9413 <54574536+jhy9413@users.noreply.github.com>
2020-10-27 14:23:24 +0900

    Update db.php

--- db.php
+++ db.php
@@ -1,5 +1,5 @@
 <?php
-        define("DB_HOST", "10.131.1.127"); //db서버
+        define("DB_HOST", "10.131.2.131"); //db서버
         define("DB_PORT", "3306");
         define("DB_USER", "root");
         define("DB_PASSWORD", "test123");
