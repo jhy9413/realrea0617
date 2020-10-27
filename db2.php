02fbe56076f733af0b31886aa758b9db7524223e
jhy9413 <54574536+jhy9413@users.noreply.github.com>
2020-10-27 14:25:29 +0900

    Create db2.php

---
+++ db2.php
@@ -0,0 +1,7 @@
+<?php
+        define("DB_HOST", "10.131.2.131"); //db서버
+        define("DB_PORT", "3306");
+        define("DB_USER", "root");
+        define("DB_PASSWORD", "test123");
+        define("DB_DATABASE", "dog");
+?>
